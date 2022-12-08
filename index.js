adafc0f96375f15fd6e837e8ef46e88cfeef1ec5
Shashank Raj <shashank3636@gmail.com>
2022-12-09 02:39:28 +0530

    third

--- index.js
+++ index.js
@@ -75,5 +75,80 @@ function operator(oper) {
 
 
 document.getElementById('myRange').addEventListener('input', function() {
-	console.log(document.getElementById('myRange').value);
+	theme(document.getElementById('myRange').value);
 });
+
+function theme(value){
+	console.log(value);
+	switch (value) {
+		case "0":
+			document.querySelector("body").classList.remove("theme2body");
+			document.getElementById("screen").classList.remove("theme2screen");
+			for(var i=0; i< document.querySelectorAll("button").length; i++){
+				document.querySelectorAll("button")[i].classList.remove("theme2button")
+			}
+			document.querySelector(".res").classList.remove("theme2res");
+			document.querySelector(".equal").classList.remove("theme2equal");
+			document.querySelector(".del").classList.remove("theme2del");
+			document.querySelector(".title").classList.remove("theme2tit");
+			document.querySelector(".calculator").classList.remove("theme2calculator");
+			document.querySelector(".slider").classList.remove("theme2slider");
+			document.querySelector("body").classList.remove("theme3body");
+			document.getElementById("screen").classList.remove("theme3screen");
+			for(var i=0; i< document.querySelectorAll("button").length; i++){
+				document.querySelectorAll("button")[i].classList.remove("theme3button")
+			}
+			document.querySelector(".res").classList.remove("theme3res");
+			document.querySelector(".equal").classList.remove("theme3equal");
+			document.querySelector(".del").classList.remove("theme3del");
+			document.querySelector(".title").classList.remove("theme3tit");
+			document.querySelector(".calculator").classList.remove("theme3calculator");
+			document.querySelector(".slider").classList.remove("theme3slider");
+			console.log("0");
+			break;
+		case "1":
+			console.log("1");
+			document.querySelector("body").classList.add("theme2body");
+			document.getElementById("screen").classList.add("theme2screen");
+			for(var i=0; i< document.querySelectorAll("button").length; i++){
+				document.querySelectorAll("button")[i].classList.add("theme2button")
+			}
+			document.querySelector(".res").classList.add("theme2res");
+			document.querySelector(".equal").classList.add("theme2equal");
+			document.querySelector(".del").classList.add("theme2del");
+			document.querySelector(".title").classList.add("theme2tit");
+			document.querySelector(".calculator").classList.add("theme2calculator");
+			document.querySelector(".slider").classList.add("theme2slider");		
+			document.querySelector("body").classList.remove("theme3body");
+			document.getElementById("screen").classList.remove("theme3screen");
+			for(var i=0; i< document.querySelectorAll("button").length; i++){
+				document.querySelectorAll("button")[i].classList.remove("theme3button")
+			}
+			document.querySelector(".res").classList.remove("theme3res");
+			document.querySelector(".equal").classList.remove("theme3equal");
+			document.querySelector(".del").classList.remove("theme3del");
+			document.querySelector(".title").classList.remove("theme3tit");
+			document.querySelector(".calculator").classList.remove("theme3calculator");
+			document.querySelector(".slider").classList.remove("theme3slider");
+		
+			break;
+		case "2":
+			console.log("2");
+			document.querySelector("body").classList.add("theme3body");
+			document.getElementById("screen").classList.add("theme3screen");
+			for(var i=0; i< document.querySelectorAll("button").length; i++){
+				document.querySelectorAll("button")[i].classList.add("theme3button")
+			}
+			document.querySelector(".res").classList.add("theme3res");
+			document.querySelector(".equal").classList.add("theme3equal");
+			document.querySelector(".del").classList.add("theme3del");
+			document.querySelector(".title").classList.add("theme3tit");
+			document.querySelector(".calculator").classList.add("theme3calculator");
+			document.querySelector(".slider").classList.add("theme3slider");		
+			
+			break;
+	
+		default:
+			break;
+	}
+}
